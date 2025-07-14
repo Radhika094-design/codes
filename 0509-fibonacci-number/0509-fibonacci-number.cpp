d3964de5070430027052fa9ca4834f69872cc44d
Radhika <radheya981@gmail.com>
2025-07-14 09:47:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (11.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
+int sum(int n  ){
+     if(n == 0)return 0;
+     if(n==1)return 1;
+     vector<int>arr(n+1);
+     arr[0] = 0;
+     arr[1] = 1;
+     for(int i = 2;i<=n;i++){
+        arr[i] = arr[i - 1]+arr[i - 2];
+     }
+return arr[n];
+}
     int fib(int n) {
-        if(n==0||n==1){
-            return n;
-
-        }
-        return fib(n-1)+fib(n-2);
+    return sum(n);
     }
 };
