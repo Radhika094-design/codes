1dddeb401c5700d1c7987f14661eb14cd83836ea
Radhika <radheya981@gmail.com>
2025-07-17 12:37:30 +0530

    Time: 0 ms (100%), Space: 11.8 MB (54.38%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,34 +4,21 @@ public:
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
 
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-
-        // Base case
-        if (obstacleGrid[0][0] == 1) return 0;
-        dp[0][0] = 1;
-
-        // Fill first row
-        for (int j = 1; j < m; j++) {
-            if (obstacleGrid[0][j] == 0 && dp[0][j - 1] == 1)
-                dp[0][j] = 1;
-        }
-
-        // Fill first column
-        for (int i = 1; i < n; i++) {
-            if (obstacleGrid[i][0] == 0 && dp[i - 1][0] == 1)
-                dp[i][0] = 1;
-        }
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        dp[1][0]  =1 ;
 
         // Fill rest of the grid
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (obstacleGrid[i][j] == 0) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (obstacleGrid[i-1][j-1] == 1) {
+                    dp[i][j] = 0;
+                }else{
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
                 }
                 // Else leave dp[i][j] = 0 by default
             }
         }
 
-        return dp[n - 1][m - 1];
+        return dp[n][m];
     }
 };
