1cc861f9b6bc766ae09879d64fb7413eccfda108
Radhika <radheya981@gmail.com>
2025-07-21 20:45:00 +0530

    Time: 3 ms (87.8%), Space: 15.5 MB (44.81%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -6,7 +6,7 @@ long long sum = 0;
 for(int i = 0; i < n; i++) {
 sum += nums[i];
 }
-if(target > sum || target < -sum || (target + sum) % 2 != 0) {
+if(target > sum || target < -sum || (target +sum) % 2 != 0) {
 return 0;
 }
 long long targetSum = (target + sum) / 2;
@@ -15,7 +15,7 @@ vector<vector<int>> dp(n + 1, vector<int>(targetSum + 1, 0));
 for(int i = 0; i <= n; i++) {
 dp[i][0] = 1;
 }
-
+  
 for(int i = 1; i <= n; i++) {
 for(int j = 0; j <= targetSum; j++) {
 dp[i][j] = dp[i-1][j];
